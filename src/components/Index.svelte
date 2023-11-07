f0efd3c9c35fb76b46d3ffe32a4d8c2f98b92bba
Michelle McGhee <michmac202@gmail.com>
2023-11-06 17:14:19 -0800

    key normalization

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,142 @@
 <script>
-	import { getContext } from "svelte";
-	import Demo from "$components/demo/Demo.svelte";
-	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
+	import MultiLine from "$components/layercake/todo/MultiLine.svelte";
+	import AxisX from "$components/layercake/AxisX.svg.svelte";
+	import AxisY from "$components/layercake/AxisY.svg.svelte";
+	import { LayerCake, Svg, flatten } from "layercake";
+	import { scaleOrdinal } from "d3-scale";
+	import jazmine from "$data/pitch/jazmine.csv";
+	import harry from "$data/pitch/harry.csv";
+	import carrie from "$data/pitch/carrie.csv";
+	import demi from "$data/pitch/demi.csv";
+	import legend from "$data/pitch/legend.csv";
+	import fergie from "$data/pitch/fergie.csv";
+	import michelle from "$data/pitch/michelle.csv";
 
-	// const copy = getContext("copy");
-	// const data = getContext("data");
+	const xKey = "timestamp";
+	const yKey = "frequency";
+	const zKey = "singer";
+	const normalizeKey = "C";
+
+	const data = [
+		// {
+		// 	singer: "carrie",
+		// 	data: carrie.map(castValues).filter(filterValues)
+		// },
+		// {
+		// 	singer: "demi",
+		// 	data: demi.map(castValues).filter(filterValues)
+		// },
+		// {
+		// 	singer: "fergie",
+		// 	data: fergie.map(castValues).filter(filterValues)
+		// },
+		{
+			singer: "harry",
+			data: harry,
+			key: "G#"
+		},
+		// {
+		// 	singer: "jazmine",
+		// 	data: jazmine.map(castValues).filter(filterValues)
+		// },
+		// {
+		// 	singer: "legend",
+		// 	data: legend.map(castValues).filter(filterValues)
+		// },
+		{
+			singer: "michelle",
+			data: michelle,
+			key: "D"
+		}
+	];
+
+	const transpose = (originalHz, originalKey, transposeKey) => {
+		const stepsToTranspose = halfStepsBetween(originalKey, transposeKey);
+		let newFrequency = originalHz * 2 ** (stepsToTranspose / 12);
+
+		const octaveShift = originalKey === "D" ? 0 : 1;
+		newFrequency *= 2 ** octaveShift;
+
+		return newFrequency;
+	};
+	const halfStepsBetween = (originalKey, transposeKey) => {
+		const keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"];
+		const originalKeyIndex = keys.indexOf(originalKey);
+		const transposeKeyIndex = keys.indexOf(transposeKey);
+		return transposeKeyIndex - originalKeyIndex;
+	};
+
+	const timeFilter = (d) => true;
+	// const timeFilter = (d) => d.timestamp < 15;
+	const groupedData = data.map((l) => ({
+		singer: l.singer,
+		values: l.data
+			.map((d) => ({
+				...d,
+				singer: l.singer,
+				frequency: +d.frequency,
+				timestamp: +d.timestamp
+			}))
+			.map((d) => ({
+				...d,
+				frequency: transpose(d.frequency, l.key, normalizeKey)
+			}))
+			.filter(timeFilter)
+	}));
+
+	const seriesNames = data.map((d) => d.singer);
+	const seriesColors = [
+		"#FF6B6B",
+		"#4ECDC4",
+		"#FFD166",
+		"#A463F2",
+		"#83E377",
+		"#FF8C94",
+		"#5DA0D1",
+		"#FFAA5C",
+		"#D3A4E6",
+		"#6BEDB5"
+	];
+	const zScale = scaleOrdinal().domain(seriesNames).range(seriesColors);
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
+<div class="chart-container">
+	<LayerCake
+		padding={{ top: 7, right: 10, bottom: 20, left: 50 }}
+		x={xKey}
+		y={yKey}
+		z={zKey}
+		yDomain={[0, null]}
+		zScale={scaleOrdinal()}
+		zRange={seriesColors}
+		flatData={flatten(groupedData, "values")}
+		data={groupedData}
+	>
+		<Svg>
+			<AxisX gridlines={false} formatTick={(d) => `${d}s`} />
+			<AxisY formatTick={(d) => `${d} Hz`} />
+			<MultiLine />
+		</Svg>
+	</LayerCake>
+</div>
+
+<div class="names">
+	{#each data as { singer }}
+		<div class="name" style={`--color: ${zScale(singer)}`}>{singer}</div>
+	{/each}
+</div>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 400px;
+	}
+	.names {
+		padding: 2rem;
+	}
+	.name {
+		width: fit-content;
+		margin: 1rem 0;
+		border-bottom: 6px solid var(--color);
+	}
+</style>
--- src/components/layercake/todo/MultiLine.svelte
+++ src/components/layercake/todo/MultiLine.svelte
@@ -3,26 +3,50 @@
 	Generates an SVG multi-series line chart. It expects your data to be an array of objects, each with a `values` key that is an array of data objects.
  -->
 <script>
-	import { getContext } from 'svelte';
+	import { getContext } from "svelte";
 
-	const { data, xGet, yGet, zGet } = getContext('LayerCake');
+	const { data, xGet, yGet, zGet, y, z } = getContext("LayerCake");
 
-	$: path = values => {
-		return 'M' + values
-			.map(d => {
-				return $xGet(d) + ',' + $yGet(d);
-			})
-			.join('L');
+	$: path = (values) => {
+		return (
+			"M" +
+			values
+				.map((d) => {
+					return $xGet(d) + "," + $yGet(d);
+				})
+				.join("L")
+		);
+	};
+
+	const segmentData = (data) => {
+		const segments = [];
+		let currentSegment = [];
+		for (const d of data) {
+			const value = $y(d);
+			if (value !== 0) {
+				currentSegment.push(d);
+			} else {
+				if (currentSegment.length > 0) {
+					segments.push(currentSegment);
+					currentSegment = [];
+				}
+			}
+		}
+		if (currentSegment.length > 0) {
+			segments.push(currentSegment);
+		}
+		return segments;
 	};
 </script>
 
-<g class="line-group">
+<g class="multiline">
 	{#each $data as group}
-		<path
-			class='path-line'
-			d='{path(group.values)}'
-			stroke="{$zGet(group)}"
-		></path>
+		{@const segments = segmentData(group.values)}
+		<g id={$z(group)}>
+			{#each segments as segment}
+				<path class="path-line" d={path(segment)} stroke={$zGet(segment[0])} />
+			{/each}
+		</g>
 	{/each}
 </g>
 
@@ -32,5 +56,6 @@
 		stroke-linejoin: round;
 		stroke-linecap: round;
 		stroke-width: 3px;
+		opacity: 0.75;
 	}
 </style>
